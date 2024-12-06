3ff922e73b457bc835720ab7bf750062984cb833
Owen Watkins <owen.watkins10@outlook.com>
2024-12-06 07:24:51 +0000

    day 6 - initial test

---
+++ day6/solution/full_test.go
@@ -0,0 +1,32 @@
+package solution_test
+
+import (
+	"fmt"
+	"testing"
+
+	"day6/solution"
+)
+
+func TestRouteFinder(t *testing.T) {
+	testCases := []struct {
+		input  [][]rune
+		output int
+	}{
+		{
+			[][]rune{
+				{'.', '#', '.'},
+				{'.', '.', '.'},
+				{'.', '^', '.'},
+			}, 2,
+		},
+	}
+
+	for idx, tc := range testCases {
+		t.Run(fmt.Sprintf("Test case %d", idx), func(t *testing.T) {
+			result := solution.RouteFinder(tc.input)
+			if result != tc.output {
+				t.Errorf("Case failed, expected %d, got %d", tc.output, result)
+			}
+		})
+	}
+}
---
+++ day6/solution/solution.go
@@ -0,0 +1,5 @@
+package solution
+
+func RouteFinder(grid [][]rune) int {
+	return 2
+}
