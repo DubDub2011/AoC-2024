529a498a7f24b1cb3cbc758d291af590ba62a051
Owen Watkins <owen.watkins10@outlook.com>
2024-12-06 08:06:39 +0000

    day6 - solution

---
+++ day6/frame/position.go
@@ -0,0 +1,34 @@
+package frame
+
+type Position struct {
+	X int
+	Y int
+}
+
+func (p Position) Right() Position {
+	return Position{
+		X: p.X + 1,
+		Y: p.Y,
+	}
+}
+
+func (p Position) Left() Position {
+	return Position{
+		X: p.X - 1,
+		Y: p.Y,
+	}
+}
+
+func (p Position) Up() Position {
+	return Position{
+		X: p.X,
+		Y: p.Y - 1,
+	}
+}
+
+func (p Position) Down() Position {
+	return Position{
+		X: p.X,
+		Y: p.Y + 1,
+	}
+}
--- day6/solution/solution.go
+++ day6/solution/solution.go
@@ -1,5 +1,88 @@
 package solution
 
-func RouteFinder(grid [][]rune) int {
-	return 2
+import (
+	"day6/frame"
+)
+
+const (
+	GuardRune          = '^'
+	WallRune           = '#'
+	AlreadyVisitedRune = 'X'
+) // runes
+
+const (
+	Up    = iota
+	Right = iota
+	Down  = iota
+	Left  = iota
+) // directions
+
+var (
+	moverFuncMap = map[int]func(frame.Position) frame.Position{
+		Up:    frame.Position.Up,
+		Right: frame.Position.Right,
+		Down:  frame.Position.Down,
+		Left:  frame.Position.Left,
+	}
+)
+
+var currDir = Up // bit lazy...
+
+func RouteFinder(points [][]rune) int {
+	grid := frame.Grid{points}
+	guardPos := findGuard(grid)
+	currDir = Up
+
+	for {
+		var finished bool
+		guardPos, finished = moveGuard(guardPos, grid)
+		if finished {
+			break
+		}
+	}
+
+	return countVisitedPositions(grid)
+}
+
+func moveGuard(pos frame.Position, grid frame.Grid) (frame.Position, bool) {
+	grid.SetPos(pos.X, pos.Y, AlreadyVisitedRune) // set current position to already visited
+
+	for {
+		targetPosition := moverFuncMap[currDir](pos)
+		targetVal := grid.GetPos(targetPosition.X, targetPosition.Y)
+		if targetVal == ' ' {
+			return targetPosition, true
+		}
+
+		if targetVal == WallRune {
+			currDir = (currDir + 1) % 4
+			continue // might get stuck in here if we somehow get boxed in, shouldn't happen though
+		}
+
+		grid.SetPos(targetPosition.X, targetPosition.Y, GuardRune)
+		return targetPosition, false
+	}
+}
+
+func findGuard(grid frame.Grid) frame.Position {
+	for x := 0; x < grid.GetLength(); x++ {
+		for y := 0; y < grid.GetWidth(); y++ {
+			if grid.GetPos(x, y) == GuardRune {
+				return frame.Position{x, y}
+			}
+		}
+	}
+	panic("could not find guard")
+}
+
+func countVisitedPositions(grid frame.Grid) int {
+	total := 0
+	for x := 0; x < grid.GetLength(); x++ {
+		for y := 0; y < grid.GetWidth(); y++ {
+			if grid.GetPos(x, y) == AlreadyVisitedRune {
+				total++
+			}
+		}
+	}
+	return total
 }
