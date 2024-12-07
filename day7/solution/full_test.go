479676e12e58632ff699a6043c88b490f2c97d18
Owen Watkins <owen.watkins10@outlook.com>
2024-12-07 10:45:02 +0000

    day 7 - add initial tests

---
+++ day7/solution/full_test.go
@@ -0,0 +1,28 @@
+package solution_test
+
+import (
+	"day7/solution"
+	"fmt"
+	"testing"
+)
+
+func TestTotalCalibrationResult(t *testing.T) {
+	testCases := []struct {
+		input  [][]int
+		output int
+	}{
+		{
+			[][]int{{3, 1, 2}}, 2,
+		},
+	}
+
+	for idx, tc := range testCases {
+		t.Run(fmt.Sprintf("Test case %d", idx), func(t *testing.T) {
+			res := solution.TotalCalibrationResult(tc.input)
+
+			if res != tc.output {
+				t.Errorf("expected %d, got %d", tc.output, res)
+			}
+		})
+	}
+}
---
+++ day7/solution/solution.go
@@ -0,0 +1,5 @@
+package solution
+
+func TotalCalibrationResult(input [][]int) int {
+	return 2
+}
