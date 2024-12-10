b37109f1ae8cebbfdaf67b1153467f4375e33cf2
Owen Watkins <owen.watkins10@outlook.com>
2024-12-10 07:19:53 +0000

    day 10 - add basic  idea

--- day10/solution/solution.go
+++ day10/solution/solution.go
@@ -1,5 +1,29 @@
 package solution
 
+import gr "day10/grid"
+
 func HeightmapRouter(heightmap [][]rune) int {
+	grid := gr.New(heightmap)
+	trailheads := getTrailheads(grid)
+
+	sum := 0
+	for _, pos := range trailheads {
+		sum += route(pos)
+	}
+
 	return 0
+}
+
+func route(startingPos gr.Position) int {
+	return 0
+}
+
+func getTrailheads(grid gr.Grid) []gr.Position {
+	trailheads := make([]gr.Position, 0)
+	for pos := range grid.Positions() {
+		if grid.GetPos(pos.X, pos.Y) == '0' {
+			trailheads = append(trailheads, pos)
+		}
+	}
+	return trailheads
 }
