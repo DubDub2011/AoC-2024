bc9c5dee0db78797ed80493d643bb6a82e307dc9
Owen Watkins <owen.watkins10@outlook.com>
2024-12-11 06:25:36 +0000

    day 11 - read input

--- day11/main.go
+++ day11/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("Input: ", readInput())
+}
+
+func readInput() []int {
+	data, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	res := make([]int, 0)
+	scanner := bufio.NewScanner(data)
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		val, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+		res = append(res, val)
+	}
+
+	return res
 }
