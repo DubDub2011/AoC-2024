84662e6fe23eefbb75d2f56e3e26556c65e318a2
Owen Watkins <owen.watkins10@outlook.com>
2024-12-13 16:25:49 +0000

    day 13 - read input

--- day13/main.go
+++ day13/main.go
@@ -1,7 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	data := readInput()
+	fmt.Println("Input: %v", data)
+}
+
+func readInput() [][]int {
+	data, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	res := make([][]int, 0)
+	scanner := bufio.NewScanner(data)
+	for {
+		puzzle := []int{}
+		scanner.Scan()
+		a := scanner.Text()
+		scanner.Scan()
+		b := scanner.Text()
+		scanner.Scan()
+		total := scanner.Text()
+
+		parts := strings.Split(a[len("Button A:"):], ",")
+		for _, part := range parts {
+			val, err := strconv.Atoi(part[len(" X+"):])
+			if err != nil {
+				panic(err)
+			}
+			puzzle = append(puzzle, val)
+		}
+
+		parts = strings.Split(b[len("Button B:"):], ",")
+		for _, part := range parts {
+			val, err := strconv.Atoi(part[len(" X+"):])
+			if err != nil {
+				panic(err)
+			}
+			puzzle = append(puzzle, val)
+		}
+
+		parts = strings.Split(total[len("Prize:"):], ",")
+		for _, part := range parts {
+			val, err := strconv.Atoi(part[len(" X="):])
+			if err != nil {
+				panic(err)
+			}
+			puzzle = append(puzzle, val)
+		}
+
+		if len(puzzle) != 6 {
+			panic(fmt.Errorf("bad input %v", puzzle))
+		}
+
+		res = append(res, puzzle)
+		if !scanner.Scan() {
+			break
+		}
+	}
+
+	return res
 }
