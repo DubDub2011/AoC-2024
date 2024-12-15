53cb04dc64aa56af5ea33262899de283d67d1f55
Owen Watkins <owen.watkins10@outlook.com>
2024-12-15 09:09:55 +0000

    day 15 - read input

--- day15/main.go
+++ day15/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	mapData, commands := readInput()
+	fmt.Printf("Map %v\n", mapData)
+	fmt.Printf("Commands %s\n", string(commands))
+}
+
+func readInput() ([][]rune, []rune) {
+	data, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	mapData := make([][]rune, 0)
+	scanner := bufio.NewScanner(data)
+	for scanner.Scan() {
+		if scanner.Text() == "" {
+			break
+		}
+		mapData = append(mapData, []rune(scanner.Text()))
+	}
+
+	commands := make([]rune, 0)
+	for scanner.Scan() {
+		commands = append(commands, []rune(scanner.Text())...)
+	}
+
+	return mapData, commands
+
 }
