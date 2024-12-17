0874849e6b7f5c68dc1b89173f43803e955f8d78
Owen Watkins <owen.watkins10@outlook.com>
2024-12-17 07:56:50 +0000

    day 17 - read input

--- day17/main.go
+++ day17/main.go
@@ -1,9 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	// I might be missing something but this feels to easy
-	// like it could be calculated by hand
-	fmt.Println("Hello world")
+	registers, instructions := readInput()
+	fmt.Printf("Registers %v\n", registers)
+	fmt.Printf("Instructions %v\n", instructions)
+}
+
+func readInput() ([]int, []int) {
+	data, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	registers := make([]int, 0, 3)
+	scanner := bufio.NewScanner(data)
+	scanner.Scan()
+	line := scanner.Text()[len("Register A: "):]
+	toInt, err := strconv.Atoi(line)
+	if err != nil {
+		panic(err)
+	}
+	registers = append(registers, toInt)
+
+	scanner.Scan()
+	line = scanner.Text()[len("Register B: "):]
+	toInt, err = strconv.Atoi(line)
+	if err != nil {
+		panic(err)
+	}
+	registers = append(registers, toInt)
+
+	scanner.Scan()
+	line = scanner.Text()[len("Register C: "):]
+	toInt, err = strconv.Atoi(line)
+	if err != nil {
+		panic(err)
+	}
+	registers = append(registers, toInt)
+
+	scanner.Scan() // empty line
+
+	scanner.Scan()
+	line = scanner.Text()[len("Program: "):]
+	instructions := make([]int, 0)
+	for _, c := range strings.Split(line, ",") {
+		toInt, err = strconv.Atoi(c)
+		if err != nil {
+			panic(err)
+		}
+		instructions = append(instructions, toInt)
+	}
+	return registers, instructions
 }
